da641c630784d4b6dcc9f2135a4861dd8cf340de
Brian McGilligan <brian@brimllc.com>
2012-10-07 19:14:16 -0400

    Reworked to handle modman import statements

--- mage-tool-package.php
+++ mage-tool-package.php
@@ -21,50 +21,71 @@
  * Read a modman file and generates a zip package that can be unzip in a magento root.
  */
 
+define('DS', DIRECTORY_SEPARATOR);
 define('BP', getcwd());
 
 $modmanConfigFilename   = BP . '/modman';
 $modConfigFilename      = BP . '/code/etc/config.xml';
 
 // Extract module name and version
-$sxml = simplexml_load_file($modConfigFilename);
-$module = current($sxml->modules->children());
-$modName = $module->getName();
-$modVersion = (string)$module->version;
-
-// read modman file to start packaging
-if (!($fp = fopen($modmanConfigFilename, 'r'))) {
-    die("Unable to read the modman config file");
+if (file_exists($modConfigFilename)) {
+    $sxml = simplexml_load_file($modConfigFilename);
+    $module = current($sxml->modules->children());
+    $modName = $module->getName();
+    $modVersion = (string)$module->version;
+} else {
+    $modName = pathinfo(BP, PATHINFO_FILENAME);
+    $modVersion = false;
 }
 
 // Create zip archive
-$packageName = $modName . '-' . $modVersion . '.zip';
+$packageName = $modName;
+if ($modVersion) {
+    $packageName .= '-' . $modVersion;
+}
+$packageName .= '.zip';
 $packagePath = BP . '/'. $packageName;
 
 echo "\nWriting package: $packageName\n";
-
 $za = new ZipArchive_Custom();
 $za->open($packagePath, ZipArchive::OVERWRITE);
 
-// process modman config file
-while(($line = fgets($fp)) !== false) {
-    preg_match('/([a-zA-z0-9\/\.\-\_]*)\s*([a-zA-z0-9\/\.\-\_]*)/i', $line, $matches);
-    $path       = $matches[1];
-    $zipPath    = $matches[2];
-
-    if (is_file(BP . '/' . $path)){
-        $za->addFile($path, $zipPath);
-    } else {
-        // dir
-        $za->addDir($path, $zipPath);
-    }
-}
+process_modman_file($za, $modmanConfigFilename);
 
 if (file_exists(BP . '/license.txt')) {
-    $za->addFile(BP . '/license.txt', 'license.txt');
+    $za->addFile(BP . '/license.txt', $modName .'-license.txt');
 }
 if (file_exists(BP . '/license.pdf')) {
-    $za->addFile(BP . '/license.pdf', 'license.pdf');
+    $za->addFile(BP . '/license.pdf', $modName . '-license.pdf');
 }
 
 $za->close();
+
+
+function process_modman_file ($za, $modmanFilename) {
+
+    $basepath = pathinfo($modmanFilename, PATHINFO_DIRNAME);
+
+    // read modman file to start packaging
+    if (!($fp = fopen($modmanFilename, 'r'))) {
+        die("Unable to read the modman config file");
+    }
+
+    // process modman config file
+    while(($line = fgets($fp)) !== false) {
+        preg_match('/([a-zA-z0-9\/\.\-\_\@]*)\s*([a-zA-z0-9\/\.\-\_]*)/i', $line, $matches);
+        $path       = $matches[1];
+        $zipPath    = $matches[2];
+
+        if ($path == '@import') {
+            process_modman_file($za, $basepath . DS . $zipPath . DS . 'modman');
+        } else {
+            if (is_file($basepath . DS . $path)){
+                $za->addFile($basepath . DS . $path, $zipPath);
+            } else {
+                // dir
+                $za->addDir($path, $zipPath);
+            }
+        }
+    }
+}
